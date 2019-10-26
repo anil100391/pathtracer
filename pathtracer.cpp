c0891fea94a9a271f3e860fa00c78b4e06d95702
Anil Kumar Singh <anil100391@gmail.com>
2019-10-26 15:36:23 +0530

    sampling random direction in hemisphere of hit normal and cornell scene spheres

--- pathtracer.cpp
+++ pathtracer.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cassert>
 #include <random>
+#include <chrono>
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
@@ -34,33 +35,30 @@ static color tracepath(scenef& world, randomNumGen& rng, const rayf &r, unsigned
         return {0, 0, 0, 0};
 
     rayf newray{h._pos, h._normal};
-    auto range = (rng.max() - rng.min());
-    float x = 1.0f * rng() / range - 0.5f;
-    float y = 1.0f * rng() / range - 0.5f;
-    float z = 1.0f * rng() / range - 0.5f;
-    vec3f rd = {x, y, z};
-    rd.normalize();
-    if ( rd % h._normal < 0 )
-        newray.d = rd * -1.0f;
+
+    vec3f w = h._normal;
+    vec3f u;
+
+    if ( w[0] > w[1] && w[0] > w[2] )
+        u = w * vec3f(0, 1, 0);
+    else if ( w[1] > w[0] && w[1] > w[2] )
+        u = w * vec3f(0, 0, 1);
     else
-        newray.d = rd;
+        u = w * vec3f(1, 0, 0);
 
-    // From wikipedia
-    // Probability of the newray
-    const float p = 1/(2*M_PI);
+    vec3f v = w * u;
 
-    // Compute the BRDF for this ray (assuming Lambertian reflection)
-    float cos_theta = newray.d % h._normal;
-    // color BRDF = material.reflectance / M_PI ;
-    color matreflectance = {1.0f, 1.0f, 1.0f, 1.0f};
-    color BRDF = matreflectance / M_PI ;
+    auto range = (rng.max() - rng.min());
+    float r1 = 2.0f * M_PI * (1.0f * (rng() - rng.min())  / range);
+    float r2 = 1.0f * (rng() - rng.min()) / range;
+    float r2s = std::sqrt(r2);
+    newray.d = u*std::cos(r1)*r2s + v*std::sin(r1)*r2s + w*std::sqrt(1 - r2);
+    newray.d.normalize();
 
-    // Recursively trace reflected light sources.
+    color emit = h._mat.diffuse() * h._mat.emission();
+    color diffuse = h._mat.diffuse();
     color incoming = tracepath(world, rng, newray, depth + 1);
-
-    color matemittance   = h._mat.diffuse() * (1 + h._mat.emission());
-    // Apply the Rendering Equation here.
-    return matemittance + (BRDF * incoming * cos_theta / p);
+    return emit + diffuse * incoming;
 }
 
 // -----------------------------------------------------------------------------
@@ -70,15 +68,24 @@ int main()
     unsigned int width  = 256;
     unsigned int height = 256;
 
-    vec3f  cameraPos = {0.0f, 0.0f, 0.0f};
+    vec3f  cameraPos = {3.672f, 0.0f, 0.0f};
+
     scenef world;
-    material purpleDiffuse({1.0f, 0.0f, 1.0f, 1.0f});
-    material greenEmissive({1.0f, 1.0f, 1.0f, 1.0f});
-    greenEmissive.setEmissive(50.0f);
+    material whiteDiffuse({1.0f, 1.0f, 1.0f, 1.0f});
+    material redDiffuse({1.0f, 0.0f, 0.0f, 1.0f});
+    material whiteEmissive({1.0f, 1.0f, 1.0f, 1.0f});
+    whiteEmissive.setEmissive(50.0f);
 
-    world << new sphere<FLOAT>( {0.5f, 0.0f, 4.5f}, 0.5f, purpleDiffuse )
-          << new sphere<FLOAT>( {-0.5f, 0.0f, 3.0f}, 0.5f, greenEmissive );
+    world << new sphere<FLOAT>(
+                 {0.4f, 0.0f, -0.6f}, 0.75f * 0.5f, whiteDiffuse )
+          << new sphere<FLOAT>(
+                 {-0.4f, 0.5f, -0.6f}, 0.75f * 0.5f, redDiffuse )
+          << new sphere<FLOAT>(
+                 {-0.4f, -0.5f, -0.6f}, 0.75f * 0.5f, redDiffuse )
+          << new sphere<FLOAT>(
+                 {0.0f, 0.0f, 1.0f}, 0.75f * 0.5f, whiteEmissive );
 
+    auto begin = std::chrono::high_resolution_clock::now();
     FILE *image = fopen( "render.ppm", "w" );
     fprintf( image, "P6\n%d %d\n255\n", width, height );
     float progPercent = 0.0f;
@@ -94,15 +101,16 @@ int main()
         for ( unsigned int ii = 0; ii < width; ++ii )
         {
             // build ray for pixel (ii, jj)
-            float px  = 1.0f * ii / ( width - 1 ) - 0.5f;
-            float py  = 1.0f * jj / ( width - 1 ) - 0.5f;
-            float pz  = 1.0f;
+            float py  = 1.0f * ii / ( width - 1 ) - 0.5f;
+            float pz  = 1.0f * (width -1 - jj) / ( width - 1 ) - 0.5f;
+            float px  = -1.0f;
             vec3f dir = {px, py, pz};
             dir.normalize();
             rayf r = {cameraPos, dir};
             color pixcolor = {0, 0, 0, 0};
-            std::mt19937 mtrng;
-            unsigned int numSamples = 512;
+            auto now = std::chrono::high_resolution_clock::now();
+            std::mt19937 mtrng((now - begin).count());
+            unsigned int numSamples = 128;
             for ( unsigned int sample = 0; sample < numSamples; ++sample )
             {
                 pixcolor = pixcolor + tracepath(world, mtrng, r, 0);
