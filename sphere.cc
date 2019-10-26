52d5d9c568affb9e264d6ca5d4e062b5c8ca7bcb
nebula <anil100391@gmail.com>
2019-10-26 12:23:02 +0530

    Sphere Ray intersection Test

--- sphere.cc
+++ sphere.cc
@@ -4,75 +4,32 @@
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 template <typename T>
-static bool solveQuadratic( const T &a, const T &b, const T &c, T &x0, T &x1 )
-{
-    T discr = b * b - 4 * a * c;
-    if ( discr < 0 )
-        return false;
-    else if ( discr == 0 )
-        x0 = x1 = -0.5 * b / a;
-    else
-    {
-        T q = ( b > 0 ) ? -0.5 * ( b + std::sqrt( discr ) )
-                        : -0.5 * ( b - std::sqrt( discr ) );
-        x0 = q / a;
-        x1 = c / q;
-    }
-    if ( x0 > x1 )
-        std::swap( x0, x1 );
-
-    return true;
-}
-
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-template <typename T>
 constexpr bool sphere<T>::intersect( const ray<T> &r, hit<T> &h ) const noexcept
 {
-    ///*
-    auto c = _center - r.o;
-    T csq = c.len2();
-
-    T b = c % r.d;
-
-    T asq = csq - b * b;
-
-    if ( asq > _radius * _radius )
+    // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+    auto op            = _center - r.o;
+    auto radiusSquared = _radius * _radius;
+    auto b             = op % r.d;
+    auto determinant   = b * b - op.len2() + radiusSquared;
+    if ( determinant < 0 )
         return false;
 
-    T trappedSegHalf = std::sqrt(_radius * _radius - asq);
-    h._pos = r.o + r.d * (b - trappedSegHalf);
-    h._normal = h._pos - _center;
-    h._normal.normalize();
-    h._mat = _mat;
+    determinant              = std::sqrt( determinant );
+    auto             minusT  = b - determinant;
+    auto             plusT   = b + determinant;
+    constexpr double Epsilon = 0.000000001;
+    if ( minusT < Epsilon && plusT < Epsilon )
+        return false;
+
+    auto t           = minusT > Epsilon ? minusT : plusT;
+    auto hitPosition = r.o + r.d * t;
+    auto normal      = ( hitPosition - _center );
+    normal.normalize();
+    bool inside = normal % r.d > 0;
+    if ( inside )
+        normal = normal * -1.0f;
+    h._pos    = hitPosition;
+    h._normal = normal;
+    h._mat    = _mat;
     return true;
-    //*/
-    /*
-  // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-  auto op = _center - r.o;
-  auto radiusSquared = _radius * _radius;
-  auto b = op % r.d;
-  auto determinant = b * b - op.len2() + radiusSquared;
-  if (determinant < 0)
-    return false;
-
-  determinant = sqrt(determinant);
-  auto minusT = b - determinant;
-  auto plusT = b + determinant;
-  constexpr double Epsilon = 0.000000001;
-  if (minusT < Epsilon && plusT < Epsilon)
-    return false;
-
-  auto t = minusT > Epsilon ? minusT : plusT;
-  auto hitPosition = r.o + r.d * t;
-  auto normal = (hitPosition - _center);
-  normal.normalize();
-  bool inside = normal % r.d > 0;
-  if (inside)
-    normal = normal * -1.0f;
-  h._pos = hitPosition;
-  h._normal = normal;
-  h._mat = _mat;
-  return true;
-  */
 }
---
+++ tests/sphereRayIntersectionTests.cpp
@@ -0,0 +1,67 @@
+#include "../ray.h"
+#include "../sphere.h"
+#include <cassert>
+#include <random>
+
+using vec3f   = vec3<float>;
+using rayf    = ray<float>;
+using spheref = sphere<float>;
+
+static std::mt19937 rng;
+static auto         range = rng.max() - rng.min();
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+float randomlength() { return ( 1.0f * rng() / range ); }
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+vec3f randomdirection()
+{
+    float x = randomlength() - 0.5f;
+    float y = randomlength() - 0.5f;
+    float z = randomlength() - 0.5f;
+    vec3f v{x, y, z};
+    v.normalize();
+    return v;
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+int main()
+{
+    constexpr vec3f   center{2.0f, 1.0f, 4.0f};
+    constexpr spheref s( center, 2.0f );
+
+    constexpr int numTrials = 100;
+
+    for ( int ii = 0; ii < numTrials; ++ii )
+    {
+        vec3f dir = randomdirection();
+
+        {
+            // ray origin outside sphere
+            vec3f randompointoutsideSphere =
+                center + dir * ( 1.0f + s.radius() ) * randomlength();
+
+            rayf       r( randompointoutsideSphere, dir * -1.0f );
+            hit<float> h;
+            bool       isec = s.intersect( r, h );
+            assert( isec );
+        }
+
+        {
+            // ray origin inside sphere
+            vec3f randompointinsideSphere =
+                center + dir * ( s.radius() ) * randomlength();
+
+            rayf       r( randompointinsideSphere, dir * 1.0f );
+            hit<float> h;
+            bool       isec = s.intersect( r, h );
+            assert( isec );
+        }
+    }
+
+    std::cout << "All tests passed\n";
+    return 0;
+}
